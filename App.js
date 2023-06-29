563cc80e4a306c5f17a9f317b7f20a1407edf514
hvferreira <heldervferreira@gmail.com>
2023-06-29 17:22:57 +0100

    add temp and styles

--- App.js
+++ App.js
@@ -4,18 +4,63 @@ import { View,Text, SafeAreaView, StyleSheet } from "react-native";
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View View style={styles.container}>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
         <Text> Current Weather</Text>
+        <Text style={styles.temp}> 6</Text>
+
+        <Text style={styles.feels}> Feels like 5</Text>
+
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}> High: 8 </Text>
+          <Text style={styles.highLow}> Low: 6</Text>
+        </View>
       </View>
+        <View style={styles.bodyWrapper}>
+          <Text style={styles.description}> Its Sunny </Text>
+          <Text style={styles.message}> Its perfect t-shirt weather</Text>
+        </View>
+
+    
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  wrapper:{
+    flex: 1,
     backgroundColor: 'pink',
   },
+  temp:{
+    color:'black',
+    fontSize: 48,
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black',
+  },
+  highLow: {
+    color:'black',
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+  justifyContent: 'flex-end',
+  alignItems: 'flex-start'
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
+  },
+
 })
 
 
